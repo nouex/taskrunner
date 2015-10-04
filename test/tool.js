efa0e823c83faea8e10d4a6ad128f7008c4a70fe
Hudson Reece <alfaroamauri@gmail.com>
2015-10-04 15:57:40 -0400

    basic tests pass

---
+++ test/tool.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var tool = require("../src/tool.js");
+
+describe("deepCpy()", function() {
+  it ("copies own enum & deep", function() {
+    var arr = [{}, {"a": {}}];
+    var cpd = tool.deepCpy(arr);
+
+    expect(cpd).not.toBe(arr);
+    expect(cpd[0]).not.toBe(arr[0]);
+    expect(cpd[1].a).not.toBe(arr[1].a)
+  })
+});
+
+describe("objForEach()", function() {
+  it ("")
+});
